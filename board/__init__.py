ede2edaa5077817a80e92ecc51727b0a16f3f509
yexi-yuan <yuanyexi1@gmail.com>
2023-12-19 15:14:53 +0800

    added create posts page

--- board/__init__.py
+++ board/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 
-from board import pages
+from board import pages, posts
 
 
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(pages.bp)
+    app.register_blueprint(posts.bp)
 
     return app
 
